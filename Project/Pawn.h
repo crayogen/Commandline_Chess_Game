a078bf40ff6adb2c57e0e752f6474aa20171895e
Hajira Zaman <hajirazam@gmail.com>
2018-12-18 20:27:17 +0500

    Revert "Initial commit"
    This reverts commit a001e1bec8ba0b2e129edd0054563b4ececdaeb9.

--- Project/Pawn.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Chess_Piece.h"
-class Pawn : public Chess_Piece
-{
-public:
-	Pawn(int teamNum, int position, Chess_Piece* board[8][8]);
-	~Pawn();
-	void getValidMoves(Chess_Piece* board[8][8]);
-};
--- Project/Rook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Chess_Piece.h"
-class Rook :public Chess_Piece
-{
-public:
-	Rook(int teamNum, int pos, Chess_Piece* board[8][8]);
-	~Rook();
-	void getValidMoves(Chess_Piece* board[8][8]);
-};
